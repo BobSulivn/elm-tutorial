768bf834520e1c3a8ca7ae60d2bac8fc85902dde
Bob Sullivan <robertemmettsullivan@gmail.com>
2019-08-07 20:24:24 -0700

    updated git ginore

--- src/forms.elm
+++ src/forms.elm
@@ -1,4 +1,3 @@
--- Testing .gitignore round 3
 module Main exposing (Model, Msg(..), init, main, passwordLengthValidation, passwordMatchValidation, update, view, viewInput, viewValidation)
 
 import Browser
